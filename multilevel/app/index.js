fa1f9c8c1ff1598d29f3f9e74ef3c25b9c1161b2
João Paulo Dubas <joao.dubas@gmail.com>
2014-02-04 20:07:04 -0200

    [add] Naive implementation to expose leveldb through multilevel.

---
+++ multilevel/app/index.js
@@ -0,0 +1,10 @@
+var net = require('net');
+var levelup = require('levelup');
+var leveldown = require('leveldown');
+var multilevel = require('multilevel');
+
+var db = levelup(process.env.NODE_DB_PATH, {db: leveldown});
+
+net.createServer(function (conn) {
+  conn.pip(multilevel.server(db).pipe(conn));
+}).listen(parseInt(process.env.NODE_PORT));
