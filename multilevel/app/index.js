99abbcc77a30df447c41a3ebe1e0712bfa2e52a8
João Paulo Dubas <joao.dubas@gmail.com>
2014-02-07 17:32:32 -0200

    [fix] Pipe should be conn -> multilevel -> conn.

--- multilevel/app/index.js
+++ multilevel/app/index.js
@@ -7,5 +7,5 @@ var multilevel = require('multilevel');
 var db = levelup(process.env.NODE_DB_PATH, {db: leveldown});
 
 net.createServer(function (conn) {
-  conn.pip(multilevel.server(db).pipe(conn));
+  conn.pipe(multilevel.server(db)).pipe(conn);
 }).listen(parseInt(process.env.NODE_PORT));
