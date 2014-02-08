5c53a00d83cd7324877b58305812393a0f5f96cd
João Paulo Dubas <joao.dubas@gmail.com>
2014-02-08 00:56:00 -0200

    [fix] Minor problems during process initialization.

--- levelhud/app/index.js
+++ levelhud/app/index.js
@@ -3,12 +3,13 @@ var net = require('net');
 var multilevel = require('multilevel');
 var levelHUD = require('levelhud');
 
-var db = levelup(process.env.NODE_DB_PATH, {db: leveldown});
+var db = multilevel.client();
 var conn = net.connect({
-  port: parseInt(process.env.DB_PORT_3001_PORT),
-  host: parseInt(process.env.DB_PORT_3001_HOST)
+  port: parseInt(process.env.DB_PORT_3001_TCP_PORT),
+  host: process.env.DB_PORT_3001_TCP_ADDR
 });
 
+console.log('Connect to multilevel through ' + process.env.DB_PORT_3001_TCP_ADDR + ':' + process.env.DB_PORT_3001_TCP_PORT);
 conn.pipe(db.createRpcStream()).pipe(conn);
 
 new levelHUD().use(db).listen(parseInt(process.env.NODE_PORT));
